4c5f34717a776ca1e422cb906bcffce008b6e44b
Boyaroslav <76032531+Boyaroslav@users.noreply.github.com>
2021-10-04 14:30:24 +0300

    Add files via upload

--- brawltoken.py
+++ brawltoken.py
@@ -1,14 +1,19 @@
 import requests
 import json
-
-api=input("to get your profile data we need a token(you can get it in https://developer.brawlstars.com). type your token:")
-playername=input("type your profile tag:")
-headers={'Authorization': 'Bearer '+api}
-answ=requests.get("https://api.brawlstars.com/v1/players/%23"+playername,headers=headers)
-name=json.loads(answ.text)
-print(answ.text)
-print("name: ",name['name'])
-print("----------")
-print("club: ",name['club']['name'])
-print('trophies: ',name['trophies'])
+while True:
+    api=input("to get your profile data we need a token(you can get it in https://developer.brawlstars.com).To quit the program type quit.Type your token:")
+    if api=='quit':
+        quit()
+    playername=input("type your profile tag:")
+    headers={'Authorization': 'Bearer '+api}
+    try:
+        answ=requests.get("https://api.brawlstars.com/v1/players/%23"+playername,headers=headers)
+        name=json.loads(answ.text)
+        print(answ.text)
+        print("name: ",name['name'])
+        print("----------")
+        print("club: ",name['club']['name'])
+        print('trophies: ',name['trophies'])
+    except:
+        print("try again:")
 
